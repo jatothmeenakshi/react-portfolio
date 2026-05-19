d595ece53d4f79e2275d32941234c4a031864926
Meenakshi <jatothmeenakshi9@gmail.com>
2026-05-19 14:21:42 +0530

    added contact and styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Skills from './Skills';
 import Footer from './Footer';
 import About from './About';
 import Projects from './Project';
+import Contact from './Contact';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <About/>
       <Skills/>
       <Projects/>
+      <Contact/>
       <Footer/>
     </div>
   );
---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+function Contact(){
+    return(
+<div className="contact">
+    <h2>Contact Me</h2>
+    <input type="text" placeholder="Your Name"/>
+    <input type="email" placeholder="Your email Name"/>
+    <textarea placeholder="Enter Your Message"></textarea>
+    <button>Submit</button>
+</div>
+    );
+}
+export default Contact;
